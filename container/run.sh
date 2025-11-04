ad4f3c7f081139d2910f37e210b72350d26e9f1c
angusforrest <personal@angusforrest.com>
2025-11-04 14:26:02 +1300

    fix /home/afo59 inside of container

--- container/run.sh
+++ container/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apptainer run -B /raid/afo59/sft/astrodrift:/data/astrodrift --writable-tmpfs --no-mount home --nv --cleanenv --cwd /data/astrodrift astrodrift.sif
+apptainer run -B /raid/afo59/sft/astrodrift:/data/astrodrift --writable-tmpfs --no-mount home --home /data/home/leo --nv --cleanenv --cwd /data/astrodrift astrodrift.sif
