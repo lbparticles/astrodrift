e6c57b167ef81d9b1d7b4dae8c2525eaf7377b1a
angusforrest <personal@angusforrest.com>
2025-11-09 18:35:13 +1300

    created methods module inside shared rust create

---
+++ shared/src/methods/mod.rs
@@ -0,0 +1,10 @@
+mod dop54;
+
+pub trait ButcherTableau {
+    const ORDER: usize;
+    const STAGES: usize;
+    const A: [[f64; 7]; 7];
+    const B: [f64; 7];
+    const B_HAT: [f64; 7]; // FIXME: Is this what they are called?
+    const C: [f64; 7];
+}
