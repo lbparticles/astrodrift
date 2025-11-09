797c5791ef9d128ffa45528269ebc13a61ae80de
angusforrest <personal@angusforrest.com>
2025-11-10 10:47:40 +1300

    got it working question mark?

--- shared/src/potentials/custom_origins.rs
+++ shared/src/potentials/custom_origins.rs
@@ -1,8 +1,9 @@
 use super::Potential;
+use libm::{floor,pow};
 
 #[derive(Clone, Copy)]
-pub struct CustomOriginsPotential {
-    pub potential: Potential,
+pub struct CustomOriginsPotential<T: Potential> {
+    pub potential: T,
     pub n: usize,
     pub xt_table: *const f64,
     pub yt_table: *const f64,
@@ -11,7 +12,7 @@ pub struct CustomOriginsPotential {
     pub dt: f64,
 }
 
-struct Quintic_Coeff {
+struct QuinticCoeff {
     a: f64,
     b: f64,
     c: f64,
@@ -20,7 +21,7 @@ struct Quintic_Coeff {
     f: f64,
 }
 
-fn quintic_interp(t: f64, coeff: Quintic_Coeff) {
+fn quintic_interp(t: f64, coeff: QuinticCoeff)->f64 {
     coeff.a * pow(t, 5.)
         + coeff.b * pow(t, 4.)
         + coeff.c * pow(t, 3.)
@@ -28,17 +29,17 @@ fn quintic_interp(t: f64, coeff: Quintic_Coeff) {
         + coeff.e * t
         + coeff.f
 }
-impl Potential for CustomOriginPotential {
+impl<T: Potential> Potential for CustomOriginsPotential<T>{
     #[inline(always)]
-    fn evaluate(&self, _t: f64, _x: f64, _y: f64, _z: f64) -> f64 {}
+    fn evaluate(&self, _t: f64, _x: f64, _y: f64, _z: f64) -> f64 {0.0}
     #[inline(always)]
-    fn force(&self, _t: f64, x: f64, y: f64, z: f64) -> (f64, f64, f64) {
-        let particles = [[0.0_f64, 1000]; 3];
+    fn force(&self, t: f64, x: f64, y: f64, z: f64) -> (f64, f64, f64) {
+        let mut particles = [[0.0_f64; 3]; 10000];
         let t0 = floor(t / self.dt) as usize;
-        for p in 0..self.n {
-            let p0: usize = p * self.n + t0;
+        for (i,p) in particles.iter_mut().enumerate().take(self.n) {
+            let p0: usize = i * self.n + t0;
             let x_coeff = unsafe {
-                QuiniticCoeff {
+                QuinticCoeff {
                     a: *self.xt_table.add(p0),
                     b: *self.xt_table.add(p0 + 1),
                     c: *self.xt_table.add(p0 + 2),
@@ -48,7 +49,7 @@ impl Potential for CustomOriginPotential {
                 }
             };
             let y_coeff = unsafe {
-                QuiniticCoeff {
+                QuinticCoeff {
                     a: *self.yt_table.add(p0),
                     b: *self.yt_table.add(p0 + 1),
                     c: *self.yt_table.add(p0 + 2),
@@ -58,7 +59,7 @@ impl Potential for CustomOriginPotential {
                 }
             };
             let z_coeff = unsafe {
-                QuiniticCoeff {
+                QuinticCoeff {
                     a: *self.zt_table.add(p0),
                     b: *self.zt_table.add(p0 + 1),
                     c: *self.zt_table.add(p0 + 2),
@@ -67,16 +68,18 @@ impl Potential for CustomOriginPotential {
                     f: *self.zt_table.add(p0 + 5),
                 }
             };
-            particles[p] = (
-                quintic_interp(t - t0, x_coeff),
-                quintic_interp(t - t0, y_coeff),
-                quintic_interp(t - t0, z_coeff),
-            );
+            *p = [
+                quintic_interp(t - (t0 as f64), x_coeff),
+                quintic_interp(t - (t0 as f64), y_coeff),
+                quintic_interp(t - (t0 as f64), z_coeff),
+            ];
         }
-        let total_force = [0.0_f64, 3];
-        for i in 0..n {
-            (&total_force[0], &total_force[1], &total_force[2]) +=
-                potential.force(t, x - particle[p], x - particle[p], z - particle[p])
+        let mut total_force = (0.0_f64,0.0_f64,0.0_f64);
+        for p in particles.iter().take(self.n) {
+            let (f1,f2,f3) = self.potential.force(t, x - p[0], y - p[1], z - p[2]);
+            total_force.0 += f1;
+            total_force.1 += f2;
+            total_force.2 += f3;
         }
         total_force
     }
