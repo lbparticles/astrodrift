30b8396900af6aaedd16ac09c9c60d9f7c92e9d1
angusforrest <personal@angusforrest.com>
2025-11-11 15:38:33 +1300

    fix naming convention

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,14 +1,20 @@
-enum PotentialEnum {
-    potential_id,
-    params,
-    lut_id,
+struct PotentialRecipe {
+    potential_id: PotentialEnum,
+    fparams: [f64;6],
+    uparams: [usize;6],
+    lutInfo: LookUpTable,
+}
+
+struct LookUpTable {
+    id: usize,
+    length: usize,
 }
 
 
-enum PotentialList {
-    bovy14,
-    plummer,
-    mn,
-    nfw,
-    sphcutoff,
+enum PotentialEnum {
+    Bovy14,
+    Plummer,
+    MN,
+    NFW,
+    SphCutoff,
 }
