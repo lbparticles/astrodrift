e949f6762ada66366d4f43dafb344b072a787783
Jack Patterson <jack.patterson.nz@gmail.com>
2025-11-12 13:10:33 +1300

    Prototype potential test setup

--- shared/src/potentials/plummer.rs
+++ shared/src/potentials/plummer.rs
@@ -1,10 +1,12 @@
 use libm::{pow};
 use crate::potentials::Potential;
+
 #[derive(Clone, Copy)]
 pub struct PlummerPotential {
     pub amp: f64,
     pub b: f64,
 }
+
 impl Potential for PlummerPotential {
     // #[inline(always)]
     // fn evaluate(&self, t: f64, x: f64, y: f64, z: f64) -> f64 {
@@ -13,8 +15,8 @@ impl Potential for PlummerPotential {
     // }
     #[inline(always)]
     fn force(&self, _t: f64, x: f64, y: f64, z: f64) -> (f64, f64, f64) {
-        let r2 = pow(x, 2.0) + pow(y, 2.0) + pow(z, 2.0);
-        let ar = -self.amp * pow(r2 + pow(self.b, 2.0), -1.5);
+        let r2 =  x*x + y*y + z*z;
+        let ar = -self.amp * pow(r2 + (self.b * self.b), -1.5);
         let ax = ar * x;
         let ay = ar * y;
         let az = ar * z;
